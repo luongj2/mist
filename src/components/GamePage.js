3f147369063645a6c687e32a4b69c5db444d486c
yahuy1 <nguyengiahuyak29@gmail.com>
2022-03-14 21:15:28 -0400

    mistDB

--- src/components/GamePage.js
+++
@@ -1,8 +0,0 @@
-import { useParams } from "react-router-dom"
-
-export default function GamePage() {
-    let params = useParams();
-    return (
-        <h1>{`Game Page (ID: ${params.id})`}</h1>
-    )
-}
