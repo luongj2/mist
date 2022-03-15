1e1f77d355025c8951ed487976ce1cf54cccacc7
Joey Luong <89662287+luongj2@users.noreply.github.com>
2022-03-14 20:50:57 -0400

    Add files via upload

---
+++ mist/about/index.php
@@ -0,0 +1,53 @@
+<html>
+
+<head>
+    <?php include(dirname(__DIR__).'/includes/html/head.html')?>    
+    
+    <title>About Mist</title>
+</head>
+
+<body>
+    <noscript>You need to enable JavaScript to access this page.</noscript>
+
+    <?php include(dirname(__DIR__).'/includes/html/header.html')?>
+
+    <link rel="stylesheet" href="about.css">
+
+    <div class="about">
+        <div class="title">
+            About Us
+        </div>
+
+        <div class="description">
+            <div class="Information">
+                </br>We serve to allow users to search and browse a variety of games in our game library through a web browser.</br>
+                <br>Users can chat and create discussions through our online community forums.</br>
+                <br>Publishers can also publish their own games alongside our expanding library.</br>
+            </div>
+        </div>
+
+        <div class="description">
+            <div class="members">
+                <li><b>Joey Luong</b> - <i>Project Manager & Full-Stack Programmer</i></li>
+                <li><b>Harrison Baker</b> - <i>Technical Manager & Front-End Programmer (HTML)</i></li>
+                <li><b>Huy Nguyen</b> - <i>Front-End Programmer (JavaScript)</i></li>
+                <li><b>Jon Kraft</b> - <i>Back-End Programmer (SQL)</i></li>
+                <li><b>Eric Liao</b> - <i>Back-End Programmer (PHP)</i></li>
+            </div>
+
+            <div class="members">
+                <li>luongj2@miamioh.edu</li>
+                <li>bakerhh@miamioh.edu</li>
+                <li>nguyenhg@miamioh.edu</li>
+                <li>kraftjk@miamioh.edu</li>
+                <li>liaoh3@miamioh.edu</li>
+            </div>
+        </div>
+
+        <div class="picture">
+            
+        </div>
+    </div>
+</body>
+
+</html>
