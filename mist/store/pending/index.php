b566a60823c769679be8531e20f3815d4d7aed1f
yahuy1 <nguyengiahuyak29@gmail.com>
2022-04-26 01:29:27 -0400

    finish game admin verification

--- mist/store/pending/index.php
+++ mist/store/pending/index.php
@@ -11,7 +11,7 @@
     }
 ?>
 
-<div>
+<form action="pending.php" method="post">
     <ul>
         <?php
             $games = callProcedure("spGetGamesFromSearch", "", "", "", "pending");
@@ -30,18 +30,29 @@
                 $compatibleMacOS = $game["compatibleMacOS"];
                 $compatibleLinux = $game["compatibleLinux"];
                 $developerName = $game["developerName"];
+                $requestID = $game["requestID"];
 
+                echo "<input style=\"display: none\" name=\"requestID\" value=$requestID>";
                 echo "<li>";
-                echo "<img src = \"data:image/png;base64,$gamePicture\"><br>";
-                echo "<h1>$gameName</h1><br>";
-                echo "<h4>$gameDescription</h4><br>";
-                echo "<p>Release Date: $gameDate</p>";
+                echo "<div class=\"aaa\">";
+                    echo "<img src = \"data:image/png;base64,$gamePicture\"><br>";
+                    echo "<h1>$gameName</h1><br>";
+                    echo "<h4>$gameDescription</h4><br>";
+                    echo "<p>Release Date: $gameDate</p>";
+                echo "</div>";
+
+                echo "<div>";
+                    echo "<textarea style=\"display: block\"name=\"requestReason\" placeholder=\"Reason\" rows=\"8\"></textarea>";
+                    echo "<br>";
+                    echo "<button name=\"accept\">Accept</button>";
+                    echo "<button name=\"deny\">Deny</button>";
+                echo "</div>";
                 echo "</li>";
                 echo "<br>";
             }
         ?>
     </ul>
-</div>
+</form>
 
 <?php
     require(dirname(__DIR__, $steps)."/footer/index.php")
---
+++ mist/store/pending/pending.php
@@ -0,0 +1,18 @@
+<?php
+    $steps = 2;
+    require(dirname(__DIR__, $steps)."/database.php");
+    require(dirname(__DIR__, $steps)."/functions.php");
+
+    if(isset($_POST["accept"])) {
+        $requestID = $_POST["requestID"];
+        $requestReason = $_POST["requestReason"];
+        callProcedure("spUpdateRequest", $requestID, "accepted", $requestReason);
+    } else if (isset($_POST["deny"])) {
+        $requestID = $_POST["requestID"];
+        $requestReason = $_POST["requestReason"];
+        callProcedure("spUpdateRequest", $requestID, "denied", $requestReason);
+    }
+
+    header("location: index.php");
+    exit();
+?>
